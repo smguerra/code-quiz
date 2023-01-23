4e515770f07b1cdc96a74f9e630cc5790dcdbe61
Milena <milenagg77@hotmail.com>
2023-01-23 23:14:47 +0000

    created choices function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-let startScreen = document.querySelector("#start-Screen");
+let startScreen = document.querySelector("#start-screen");
 let questions = document.querySelector("#questions");
 let startButton = document.querySelector("#start");
 let time = document.querySelector("#time");
@@ -16,7 +16,36 @@ function quizStart() {
 }
 startButton.addEventListener("click", quizStart);
 
-function retrieveQuestion(){
-    let questionTitle = document.querySelector("question-title");
-    
+function retrieveQuestion() {
+    let questionTitle = document.getElementById("question-title");
+    questionTitle.textContent = questionsQuiz[0].title;
 }
+
+// Select the element to append the buttons
+let choicesElement = document.getElementById("choices");
+
+function displayChoices(question) {
+
+  // Iterate through the choices of the current question
+  for (var i = 0; i < question.choices.length; i++) {
+    // Get the current choice
+    let choice = question.choices[i];
+
+    // Create a button element
+    let button = document.createElement("button");
+
+    // Set the text content of the button
+    button.textContent = choice;
+
+    // Append the button to the choices element
+    choicesElement.appendChild(button);
+  }
+}
+
+// Index to keep track of the current question
+let currentQuestionIndex = 1;
+
+// Display the choices for the first question
+displayChoices(questionsQuiz[currentQuestionIndex]);
+
+
--- assets/js/question.js
+++ assets/js/question.js
@@ -2,20 +2,25 @@ let questionsQuiz = [
     {
         title: "Inside which HTML element do we put the JavaScript?",
         choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
-        answer: "<script>",
-
+        answer: "<script>"
+    },    
+    {
         title: "Which of these is NOT a data type?",
         choices: ["numbers", "booleans", "string", "alert"],
-        answer: "alert",
-
+        answer: "alert"
+    },
+    {
         title: "JavaScript is the same as Java",
         choices: ["true", "false"],
-        answer: "false",
-
+        answer: "false"
+    },
+    {
         title: "How do you declare a JavaScript variable?",
         choices: ["variable carName", "var carName", "v carName"],
         answer: "var carName",
 
+    },
+    { 
         title: "Which operator is used to assign a value to a variable?",
         choices: ["+", "=", "*", "-"],
         answer: "=",
