992ec5fc1c17fb5354619976ddc89b7a6933b41d
Milena <milenagg77@hotmail.com>
2023-01-23 12:12:04 +0000

    created function quizStart

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,21 @@
-alert("hola");
+let startScreen = document.querySelector("#start-Screen");
+let questions = document.querySelector("#questions");
+let startButton = document.querySelector("#start");
+let time = document.querySelector("#time");
+let timeCount = 60;
+let timer
+
+function quizStart() {
+    startScreen.setAttribute("class", "hide");
+    questions.removeAttribute("class");
+    timer = setInterval(() => {
+        time.textContent = timeCount;
+        timeCount--;
+    }, 1000);
+    retrieveQuestion()
+}
+startButton.addEventListener("click", quizStart);
+
+function retrieveQuestion(){
+    let questionTitle = document.querySelector("question-title")
+}
