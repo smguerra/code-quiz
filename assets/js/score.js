ee3f8015bc7e3e6e652630793fab13200b3a0c14
Milena <milenagg77@hotmail.com>
2023-01-24 13:29:49 +0000

    added savescore function

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,19 +1,19 @@
-// let highScoresList = document.getElementById("highscores");
-// let clearButton = document.getElementById("clear");
+let highScoresList = document.getElementById("highscores");
+let clearButton = document.getElementById("clear");
 
-// function displayHighScores() {
-//   let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
-//   highScores.sort((a, b) => b.score - a.score);
-//   highScores.forEach((score, i) => {
-//     let li = document.createElement("li");
-//     li.textContent = `${i + 1}. ${score.initials} - ${score.score}`;
-//     highScoresList.appendChild(li);
-//   });
-// }
+function displayHighScores() {
+  let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.forEach((score, i) => {
+    let li = document.createElement("li");
+    li.textContent = `${i + 1}. ${score.initials} - ${score.score}`;
+    highScoresList.appendChild(li);
+  });
+}
 
-// clearButton.onclick = function () {
-//   localStorage.removeItem("highscores");
-//   highScoresList.innerHTML = "";
-// };
+clearButton.onclick = function () {
+  localStorage.removeItem("highscores");
+  highScoresList.innerHTML = "";
+};
 
-// displayHighScores();
+displayHighScores();
